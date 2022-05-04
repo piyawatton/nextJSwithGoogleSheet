e9c73c36d698b8d68049de285a3e06daa5fcf4b5
piyawatton <piyawat.chutimavutikul@gmail.com>
2022-05-04 23:54:41 +0700

    overview infomation

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,19 +1,31 @@
 import React from 'react';
-import { Card, Col, Row, Input, Switch, Checkbox, Button, Modal, Form, Space, Spin } from 'antd';
+import { Card, Col, Row, Input, Switch, Checkbox, Button, Modal, Tag, Spin, Divider } from 'antd';
 import 'antd/dist/antd.css';
 import './App.css';
-import { editHouse, getHouses, HousePayload, toggleAllowedHouse, toggleCheckHouse, toggleEmptyHouse } from './services';
+import { getHouses, HousePayload, toggleAllowedHouse, toggleCheckHouse, toggleEmptyHouse } from './services';
+import { InfoOutlined } from '@ant-design/icons';
 const { Search } = Input
 function App() {
   const [houseList, setHouseList] = React.useState<HousePayload[]>([]);
   const [textSearch, setTextSearch] = React.useState<string>('')
   const [isLoading, setIsLoading] = React.useState<boolean>(false)
-  const [isCreating, setIsCreateing] = React.useState<boolean>(false)
+  const [isSetting, setIsSetting] = React.useState<boolean>(false)
+
+  const [overview, setOverview] = React.useState({
+    totalChecked: 0,
+    totalEmpty: 0,
+    totalNotAllow: 0,
+  })
 
   const doCallGetHouse = async () => {
     setIsLoading(true)
     const response = await getHouses()
     setHouseList(response)
+    setOverview({
+      totalChecked: response.filter((_h) => _h.checked === 1).length,
+      totalEmpty: response.filter((_h) => _h.isEmpty === 1).length,
+      totalNotAllow: response.filter((_h) => _h.allowed !== 1).length,
+    })
     setIsLoading(false)
   }
 
@@ -24,13 +36,6 @@ function App() {
   const filterHouse = (house: HousePayload) => {
     if (textSearch === '') return true
     return `${house.houseNumber}`.indexOf(textSearch) !== -1
-  }
-
-  const updateHouse = async (house: HousePayload) => {
-    setIsLoading(true)
-    const response = await editHouse(house)
-    doCallGetHouse()
-    setIsLoading(false)
   }
 
   const callToggleCheckHouse = async (house: HousePayload) => {
@@ -57,14 +62,24 @@ function App() {
   return (
     <div className="App">
       <div>
-        <div style={{ margin: '20px 0px', position: 'fixed', top: 0, right: 0, left: 0, zIndex: 1000 }}>
+        <div style={{
+          padding: '20px 0px',
+          position: 'fixed',
+          top: 0, right: 0, left: 0,
+          zIndex: 1000,
+          background: 'white'
+        }}>
           <Search size='large' allowClear onSearch={(value) => { setTextSearch(value) }} />
+          <br />
+          <h2 style={{ margin: 0 }}>
+            เปรมประชา ซีซั่น
+          </h2>
         </div>
         <Spin spinning={isLoading}>
-          <Row gutter={[16, 16]} style={{ marginTop: 80 }}>
+          <Row gutter={[16, 16]} style={{ marginTop: 100 }}>
             {houseList.filter(filterHouse).map((house) => {
               return (
-                <Col span={12}>
+                <Col span={12} md={8}>
                   <Card>
                     <div style={{ fontSize: '1.2rem' }}>
                       บ้านเลขที่ {house.houseNumber}
@@ -72,7 +87,7 @@ function App() {
                     <div style={{ marginTop: 20 }}>
                       <Switch
                         style={{ transform: 'scale(1.4)' }}
-                        checkedChildren="ตรวจแล้ว" unCheckedChildren="ยังไม่ตรวจ"
+                        checkedChildren="ได้ข้อมูลแล้ว" unCheckedChildren="ยังไม่ได้ทำ"
                         checked={house.checked === 1}
                         onChange={(checked) => { callToggleCheckHouse(house) }}
                       />
@@ -89,7 +104,7 @@ function App() {
                     <div style={{ marginTop: 25 }}>
                       <Checkbox
                         style={{ transform: 'scale(1.3)' }}
-                        checked={house.allowed === 1}
+                        checked={house.allowed !== 1}
                         onChange={() => { callToggleAllowedHouse(house) }}
                       >
                         ไม่สะดวกให้ข้อมูล
@@ -102,40 +117,31 @@ function App() {
           </Row>
         </Spin>
       </div>
-      {/* <Button shape='circle' size='large' type='primary'
-        onClick={() => { setIsCreateing(true) }}
+      <Button shape='circle' size='large' type='primary'
+        onClick={() => { setIsSetting(true) }}
         style={{
           padding: 5,
           position: 'fixed',
           bottom: 0,
           right: 0,
-          transform: 'translate(-100%,-100%) scale(1.4)',
+          transform: 'translate(-100%,-200%) scale(1.4)',
         }} >
-        เพิ่ม
-      </Button> */}
-      <Modal visible={isCreating} onCancel={() => { setIsCreateing(false) }}>
-        <Form>
-          <Form.Item>
-            <Space>
-              บ้านเลขที่
-              <Input />
-            </Space>
-          </Form.Item>
-          <Form.Item>
-            <Space>
-              <Switch
-                style={{ transform: 'scale(1.4)' }}
-                checkedChildren="ตรวจแล้ว" unCheckedChildren="ยังไม่ตรวจ"
-              />
-            </Space>
-          </Form.Item>
-          <Form.Item>
-            <Switch
-              style={{ transform: 'scale(1.4)' }}
-              checkedChildren="มีคนอยู่" unCheckedChildren="ไม่มีคนอยู่"
-            />
-          </Form.Item>
-        </Form>
+        <InfoOutlined />
+      </Button>
+      <Modal centered visible={isSetting} onCancel={() => { setIsSetting(false) }} footer={null} >
+        <Row gutter={[16, 16]}>
+          <Col span={12} style={{ textAlign: 'right' }}>ได้ข้อมูลแล้ว</Col>
+          <Col span={12}>{overview.totalChecked}/{houseList.length}</Col>
+          <Col span={12} style={{ textAlign: 'right' }}>ไม่มีคนอยู่</Col>
+          <Col span={12}>{overview.totalEmpty}/{houseList.length}</Col>
+          <Col span={12} style={{ textAlign: 'right' }}>ไม่สะดวกให้ข้อมูล</Col>
+          <Col span={12}>{overview.totalNotAllow}/{houseList.length}</Col>
+          <Divider />
+          <Col span={24} style={{ textAlign: 'center' }}>บ้านที่ยังไม่ได้ทำ</Col>
+          <Col span={24} style={{ textAlign: 'center' }}>
+            {houseList.filter((house) => house.checked !== 1).map((house) => <Tag>{house.houseNumber}</Tag>)}
+          </Col>
+        </Row>
       </Modal>
     </div>
   );
